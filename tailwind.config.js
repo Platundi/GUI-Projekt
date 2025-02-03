d7fcdfda0d645896181d9c6a2fe8fd9febf7bdf3
Thomas Stolz <152648240+Platundi@users.noreply.github.com>
2025-02-03 21:51:39 +0100

    Add Navbar, Main-Content, Footer, color and responsive design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{html,js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ["./index.html", "./src/**/*.{html,js,jsx,ts,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				primary: "#003F88", // Blau
+				primary_light: "#99c9ff", // Hellblau
+				secondary: "#FF6F00", // Orange
+			},
+		},
+	},
+	plugins: [],
 };
