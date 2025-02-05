5e0491d20063a65bb2b664bc3c2123eb78e036dc
Thomas Stolz <152648240+Platundi@users.noreply.github.com>
2025-02-05 17:13:11 +0100

    Add contact site

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./index.html", "./src/**/*.{html,js,jsx,ts,tsx}"],
-	theme: {
-		extend: {
-			colors: {
-				primary: "#003F88", // Blau
-				primary_light: "#99c9ff", // Hellblau
-				secondary: "#FF6F00", // Orange
-				secondary_focus: "#cc5800", // Dunkleres Orange
-			},
-		},
-	},
-	plugins: [],
+  content: ["./*.html", "./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#003F88", // Blau
+        primary_light: "#99c9ff", // Hellblau
+        secondary: "#FF6F00", // Orange
+        secondary_focus: "#cc5800", // Dunkleres Orange
+      },
+    },
+  },
+  plugins: [],
 };
